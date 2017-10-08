2867e6bd9d3073419ec8915299845d4a2ecc0b2c
chihoang <chihoang@chis-imac.hsd1.ca.comcast.net>
2017-10-07 17:15:57 -0700

    Start GUI with swing
    Add basic swing components to the menu

--- src/com/bank/exceptions/InvalidAccountTypeException.java
+++ src/com/bank/exceptions/InvalidAccountTypeException.java
@@ -2,6 +2,8 @@ package com.bank.exceptions;
 
 public class InvalidAccountTypeException extends Exception {
 
+	private static final long serialVersionUID = 1L;
+
 	public InvalidAccountTypeException() {
 		super("InvalidAccountTypeException");
 	}
